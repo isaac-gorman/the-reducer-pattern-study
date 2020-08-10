a0b346e2623962b2ee2373d1faf8aade9d9d3618
Isaac Gorman <emailme@isaacg.net>
2020-08-10 12:54:31 -0500

    Initial notes

---
+++ study/src/notes/overview.js
@@ -0,0 +1,12 @@
+// Learning Module Summary
+
+//  As data increasses, it becomes harder to store and effecienlty prefom logic on data within componets.
+// As application grow larger it become very time consuming  to deal with larger components and larger data sets, thus storing and transporting state becomes more difficult the larger the application becomes.
+// One solution to deal with this complexity of state and data across ever growing applications is to use "Reducers" within React components.
+// When dealing with large and complex application you want to begin to enable state to be accessed golobally. Engineers can begin to manage and access state globally by using a combination of "Context API's" and "Reducers".
+// Now I will begin to start learning about Managing Data at the global state?
+
+// Learning Outcomes
+// - I will have the ability to atriculate what the concept of "immutablity" is in programming and demonstate its benifits
+// - Define and use Reducer Functions
+// - Have the ability to use the "useReducer" hook to manage state in a component
